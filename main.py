352bb9d155abb0e3b9ed3e0694e3cff38523b40a
elmi <shahrokhelmi@gmail.com>
2021-08-06 13:40:11 +0430

    add properties to class

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 # main.py
+import math
+from math import sin, cos, pi
 
 
+class Polygon:
+    def __init__(self, n, R):
+        self.n = n
+        self.R = R
+        self.angle = (n - 2) * 180 / n
+        self.s = 2 * R * sin(pi / n)
+        self.a = R * cos(pi / n)
+        self.area = 0.5 * n * self.s * self.a
+        self.perimeter = n * self.s
+
+
+    
