fa40ad9da95fcc1fc71df8d004b1f5d60fa4cf41
elmi <shahrokhelmi@gmail.com>
2021-08-06 13:50:11 +0430

    add __repr__ to main

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ class Polygon:
         self.n = n
         self.R = R
         self.angle = (n - 2) * 180 / n
-        self.s = 2 * R * sin(pi / n)
-        self.a = R * cos(pi / n)
+        self.s = 2 * R * sin(pi / n)  # edge length
+        self.a = R * cos(pi / n)  # apothem
         self.area = 0.5 * n * self.s * self.a
         self.perimeter = n * self.s
 
+    def __repr__(self):
+        return f'Polygon(edges={self.n}, interior_angle={self.angle},' \
+               f'apothem={self.a}, area={self.area}, perimeter={self.perimeter})'
 
-    
