e2af3151ec7b35889f4d865fd19575a6617f3b82
elmi <shahrokhelmi@gmail.com>
2021-08-07 14:31:35 +0430

    add test def

--- main.py
+++ main.py
@@ -5,25 +5,125 @@ from math import sin, cos, pi
 
 class Polygon:
     def __init__(self, n, R):
-        self.n = n  # edges / vertices
-        self.R = R  # circumradius
-        self.angle = (n - 2) * 180 / n
-        self.s = 2 * R * sin(pi / n)  # edge length
-        self.a = R * cos(pi / n)  # apothem
-        self.area = 0.5 * n * self.s * self.a
-        self.perimeter = n * self.s
+        self._n = n
+        self._R = R
 
     def __repr__(self):
-        return f'Polygon(edges={self.n}, interior_angle={self.angle:0.3f},' \
-               f'apothem={self.a:0.3f}, area={self.area:0.3f}, perimeter={self.perimeter:0.3f})'
+        return f'Polygon(n={self._n}, R={self._R})'
+
+    @property
+    def count_vertices(self):
+        return self._n
+
+    @property
+    def count_edges(self):
+        return self._n
+
+    @property
+    def circumradius(self):
+        return self._R
+
+    @property
+    def interior_angle(self):
+        return (self._n - 2) * 180 / self._n
+
+    @property
+    def side_length(self):
+        return 2 * self._R * sin(pi / self._n)
+
+    @property
+    def apothem(self):
+        return self._R * cos(pi / self._n)
+
+    @property
+    def area(self):
+        return self._n / 2 * self.side_length * self.apothem
+
+    @property
+    def perimeter(self):
+        return self._n * self.side_length
 
     def __eq__(self, other):
-        if isinstance(other, Polygon):
-            return self.n == other.n and self.R == other.R
+        if isinstance(other, self.__class__):
+            return (self.count_edges == other.count_edges
+                    and self.circumradius == other.circumradius)
+        else:
+            return NotImplemented
 
     def __gt__(self, other):
-        if isinstance(other, Polygon):
-            return self.n > other.n
+        if isinstance(other, self.__class__):
+            return self.count_vertices > other.count_vertices
+        else:
+            return NotImplemented
 
+
+
+
+
+def test_polygon():
+    rel_tol = 0.001
+    abs_tol = 0.001
+    n = 3
+    R = 1
+    p = Polygon(n, R)
+    assert str(p) == f'Polygon(n=3, R=1)', f'actual: {str(p)}'
+    assert p.count_vertices == n, (f'actual: {p.count_vertices}, '
+                                   f'expected: {n}')
+    assert p.count_edges == n
+    assert p.circumradius == R
+    assert p.interior_angle == 60
+
+    n =4
+    R = 1
+    p = Polygon(n, R)
+    assert math.isclose(p.interior_angle, 90)
+    assert math.isclose(p.area, 2.0,
+                        rel_tol=rel_tol,
+                        abs_tol=abs_tol), (f'actual: {p.area}, '
+                                           f'expected: {2.0}')
+
+    assert math.isclose(p.side_length, math.sqrt(2))
+    assert math.isclose(p.perimeter, 4 * math.sqrt(2))
+    assert math.isclose(p.apothem, 0.707, rel_tol=rel_tol, abs_tol=abs_tol)
+
+    n = 6
+    R = 2
+    p = Polygon(n, R)
+    assert math.isclose(p.interior_angle, 120)
+    assert math.isclose(p.area, 10.3923,
+                        rel_tol=rel_tol,
+                        abs_tol=abs_tol), (f'actual: {p.area}, '
+                                           f'expected: {10.3923}')
+
+    assert math.isclose(p.side_length, 2, rel_tol=rel_tol, abs_tol=abs_tol )
+    assert math.isclose(p.perimeter, 12,  rel_tol=rel_tol, abs_tol=abs_tol)
+    assert math.isclose(p.apothem, 1.73205, rel_tol=rel_tol, abs_tol=abs_tol)
+
+
+    n = 12
+    R = 3
+    p = Polygon(n, R)
+    assert math.isclose(p.interior_angle, 150)
+    assert math.isclose(p.area, 27,
+                        rel_tol=rel_tol,
+                        abs_tol=abs_tol), (f'actual: {p.area}, '
+                                           f'expected: {2.0}')
+
+    assert math.isclose(p.side_length, 1.55291, rel_tol=rel_tol, abs_tol=abs_tol )
+    assert math.isclose(p.perimeter, 18.635,  rel_tol=rel_tol, abs_tol=abs_tol)
+    assert math.isclose(p.apothem, 2.89778, rel_tol=rel_tol, abs_tol=abs_tol)
+
+    p1 = Polygon(3, 100)
+    p2 = Polygon(10, 10)
+    p3 = Polygon(15, 10)
+    p4 = Polygon(15, 100)
+    p5 = Polygon(15, 100)
+
+    assert p2 > p1
+    assert p2 < p3
+    assert p3 != p4
+    assert p1 != p4
+    assert p4 == p5
+test_polygon()
 
 
